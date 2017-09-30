709157a2c175f99ef03548f597589b31ac80a51b
Dustin <dyelland@gmail.com>
2017-09-30 15:53:02 -0500

    added test for finding min value

--- test/binarySearchTree.test.js
+++ test/binarySearchTree.test.js
@@ -2,9 +2,16 @@ const { Tree } = require('./test.helper');
 const { expect } = require('chai');
 
 describe('BST Tests',function(){
+  beforeEach(function(){
+    return this.newNode = Tree.root;
+  });
   describe('appendChild',function(){
-    it('should return the newNode',function(){
-      let newNode = Tree.root.appendChild({key: 10,data: 'data'});
+    afterEach(function(){
+      this.newNode.left = null;
+      this.newNode.right = null;
+    });
+    it('should append and return the newNode',function(){
+      let newNode = this.newNode.appendChild({key: 10,data: 'data'});
       expect(Tree.root.left).to.be.null;
       expect(Tree.root.right).to.be.equal(newNode);
 
@@ -13,6 +20,12 @@ describe('BST Tests',function(){
 
       newNode = Tree.root.appendChild({key: 7,data: 'data'});
       expect(Tree.root.right.left).to.be.equal(newNode);
+    });
+    it('should return the ',function(){
+      this.newNode.appendChild({key: 3, data: 'data'});
+      this.newNode.appendChild({key: 1, data: 'data'});
+      let minValue = this.newNode.min();
+      expect(minValue).to.be.equal(1);
     });
   });
 });
